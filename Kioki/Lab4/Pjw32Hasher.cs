40be385073f5b00669440f18e60d6dbc9e59215d
Dalival <egorfedorenko.w@gmail.com>
2022-02-16 12:57:11 +0300

    Add PJW32 hasher

---
+++ Kioki/Lab4/Pjw32Hasher.cs
@@ -0,0 +1,20 @@
+﻿namespace Lab4;
+
+public static class Pjw32Hasher
+{
+    public static uint GetHash(string message)
+    {
+        uint hash = 0;
+        foreach (var symbol in message)
+        {
+            hash = (hash << 4) + (byte)symbol;
+            var h1 = hash & 4026531840;
+            if (h1 != 0)
+            {
+                hash = (hash ^ (h1 >> 24)) & 268435455;
+            }
+        }
+
+        return hash;
+    }
+}
---
+++ Kioki/Lab4/Program.cs
@@ -0,0 +1,7 @@
+﻿using Lab4;
+
+var message = File.OpenText("message.txt").ReadToEnd();
+var hash = Pjw32Hasher.GetHash(message);
+Console.WriteLine("MESSAGE: " + message);
+Console.WriteLine("HASH: " + hash);
+Console.WriteLine("HEX HASH: 0x" + hash.ToString("X"));
